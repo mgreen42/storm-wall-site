60b8769ac3eded00c33d0811217220ae99cc45f0
mfogelso <34778060+mgreen42@users.noreply.github.com>
2023-11-04 14:01:50 -0400

    add firestore and login/logout
    add firestore storage for public access variables and add login and logout pages functionality

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom'
 import React from 'react'
+import { useLogout } from '../hooks/useLogout'
 
 // styles & images
 import './Navigation.css'
 import StormWall from '../assets/stormwall-logo.svg'
 
 export default function Navigation() {
+    const { logout, isPending } = useLogout()
+
   return (
     <div className="navbar">
       <ul>
@@ -16,7 +19,8 @@ export default function Navigation() {
         <li><Link to="/login">Login</Link></li>
         <li><Link to="/signup">Sign Up</Link></li>
         <li>
-            <button className="btn">Logout</button>
+            {!isPending && <button className="btn" onClick={ logout }>Logout</button>}
+            {isPending && <button className="btn" disabled >Logging Out...</button>}
         </li>
       </ul>
     </div>
--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,36 +1,40 @@
 import { useEffect, useState } from 'react'
-import { projectAuth } from '../firebase/config'
+import { projectAuth, projectFirestore } from '../firebase/config'
 import { useAuthContext } from './useAuthContext'
 
 export const useLogout = () => {
   const [isCancelled, setIsCancelled] = useState(false)
   const [error, setError] = useState(null)
   const [isPending, setIsPending] = useState(false)
-  const { dispatch } = useAuthContext()
+  const { dispatch, user } = useAuthContext()
   
   const logout = async () => {
     setError(null)
     setIsPending(true)
 
     try {
-      // sign the user out
-      await projectAuth.signOut()
-      
-      // dispatch logout action
-      dispatch({ type: 'LOGOUT' })
+        //set status to offline
+        const uid = user.uid
+        await projectFirestore.collection('users').doc(uid).update({ online: false })
 
-      // update state
-      if (!isCancelled) {
-        setIsPending(false)
-        setError(null)
-      } 
-    } 
-    catch(err) {
-      if (!isCancelled) {
-        setError(err.message)
-        setIsPending(false)
-      }
-    }
+        // sign the user out
+        await projectAuth.signOut()
+        
+        // dispatch logout action
+        dispatch({ type: 'LOGOUT' })
+
+        // update state
+        if (!isCancelled) {
+            setIsPending(false)
+            setError(null)
+        } 
+        } 
+        catch(err) {
+        if (!isCancelled) {
+            setError(err.message)
+            setIsPending(false)
+        }
+        }
   }
 
   useEffect(() => {
