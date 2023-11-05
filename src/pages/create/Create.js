b66150f0117cf3e976e09563ce34130611997218
mfogelso <34778060+mgreen42@users.noreply.github.com>
2023-11-05 10:53:12 -0500

    add create project page
    Add the ability to create new projects and store them in firestore

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,10 +1,137 @@
+import { useState } from 'react'
+import Select from 'react-select'
+import { useCollection } from '../../hooks/useCollection'
+import { useEffect } from 'react'
+import { timestamp } from '../../firebase/config'
+import { useFirestore } from '../../hooks/useFirestore'
+import { useAuthContext } from '../../hooks/useAuthContext'
+import { useNavigate } from 'react-router-dom'
+
 // styles
 import './Create.css'
 
+// category array
+const categories = [
+    { value: 'development', label: 'Development' },
+    { value: 'design', label: 'Design' },
+    { value: 'sales', label: 'Sales' },
+    { value: 'marketing', label: 'Marketing' },
+]
+
 export default function Create() {
-  return (
-    <div>
-      Create
-    </div>
-  )
+    const navigate = useNavigate()
+    const { addDocument, response } = useFirestore('projects')
+    const { documents } = useCollection('users')
+    const { user } = useAuthContext()
+    const [users, setUsers] = useState([])
+    const [name, setName] = useState('')
+    const [details, setDetails] = useState('')
+    const [dueDate, setDueDate] = useState('')
+    const [category, setCategory] = useState('')
+    const [assignedUsers, setAssignedUsers] = useState([])
+    const [formError, setFormError] = useState(null)
+
+    // updates potential user assignment from Firestore
+    useEffect(() => {
+        if(documents) {
+            const options = documents.map(user => {
+                return {value: user, label: user.displayName}
+            })
+            setUsers(options)
+        }
+    }, [documents])
+
+    // handle form submission
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setFormError(null)
+
+        if(!category) {
+            setFormError('Please select a project category')
+            return
+        }
+
+        if(assignedUsers.length < 1) {
+            setFormError('Please assign the project to at least one user')
+            return
+        }
+
+        const createdBy = {
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+            id: user.uid
+        }
+
+        const assignedUsersList = assignedUsers.map((u) => {
+            return {
+                displayName: u.value.displayName,
+                photoURL: u.value.photoURL,
+                id: u.value.id
+            }
+        })
+
+        const project = {
+            name,
+            details,
+            category: category.value,
+            dueDate: timestamp.fromDate(new Date(dueDate)),
+            comments: [],
+            createdBy,
+            assignedUsersList
+        }
+
+        await addDocument(project)
+        if(!response.error) {
+            navigate('/')
+        }
+    } // end handle submission
+
+    // return Create page
+    return (
+        <div className="create-form">
+        <h2 className="page-title">Create a New Project</h2>
+        <form onSubmit={handleSubmit}>
+            <label>
+                <span>Project name</span>
+                <input
+                    required
+                    type="text"
+                    onChange={(e) => setName(e.target.value)}
+                    value = {name}
+                />
+            </label>
+            <label>
+                <span>Project details</span>
+                <textarea
+                    required
+                    type="text"
+                    onChange={(e) => setDetails(e.target.value)}
+                    value = {details}
+                />
+            </label>
+            <label>
+                <span>Set due date:</span>
+                <input
+                    required
+                    type="date"
+                    onChange={(e) => setDueDate(e.target.value)}
+                    value = {dueDate}
+                />
+            </label>
+            <label>Project category:</label>
+            <Select 
+                onChange={(option) => setCategory(option)}
+                options={categories}
+            />
+            <label>Assign to:</label>
+            <Select 
+                onChange={(option) => setAssignedUsers(option)}
+                options={users}
+                isMulti
+            />
+            <button className="btn form-btn">Submit</button>
+            {formError && <p className="error">{formError}</p>}
+        </form>
+        </div>
+    ) // end Create page
 }
