8a854221b3beecd6343a293af7865ebecfd7af94
mfogelso <34778060+mgreen42@users.noreply.github.com>
2023-11-06 13:17:27 -0500

    add project summary page

---
+++ src/components/ProjectList.js
@@ -0,0 +1,31 @@
+import { Link } from 'react-router-dom'
+
+// styles
+import './ProjectList.css'
+
+import React from 'react'
+import Avatar from './Avatar'
+
+export default function ProjectList({ projects }) {
+
+    return (
+        <div className='project-list'>
+            {projects.length === 0 && <p>No projects here</p>}
+            {projects.map(project => (
+                <Link to={`/projects/${project.id}`} key={project.id}>
+                    <h4>{project.name}</h4>
+                    <p>Due by {project.dueDate.toDate().toDateString()}</p>
+                    <div className='assigned-to'>
+                    <ul>
+                        {project.assignedUsersList.map(user => (
+                            <li key={user.photoURL}>
+                                <Avatar src={user.photoURL} />
+                            </li>
+                        ))}
+                    </ul>
+                    </div>
+                </Link>
+            ))}
+        </div>
+    )
+}
